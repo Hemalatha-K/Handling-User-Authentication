b2002e0637f47fa303c113d17ac0379d9e2e7526
Hemalatha-K <hema18ec039@rmkcet.ac.in>
2022-09-05 10:39:57 +0530

    First commit

---
+++ HandlingUserAuthentication/src/main/java/com/UserAuthentication.java
@@ -0,0 +1,16 @@
+package com;
+
+public class UserAuthentication {
+	public String getUserName() {
+		return "hema";
+	}
+	public String getEmail() {
+		return "hema@gmail.com";
+	}
+	public int getPhnNum() {
+		return 123456789;
+	}
+	public String getPassword() {
+		return "hema@123";
+	}
+}
